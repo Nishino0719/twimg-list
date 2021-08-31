8082314a2e752d470985c97d8b96a209a1801a88
Nishino0719 <nishino0719@icloud.com>
2021-08-31 21:55:25 +0900

    :hammer: add theme component

--- src/pages/[userName].tsx
+++ src/pages/[userName].tsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 import { useRouter } from 'next/dist/client/router'
 import React, { useContext, useEffect, useState } from 'react'
 import { Search } from '../components/search'
+import { Theme } from '../components/theme'
 import { lightModeContext } from '../context/theme'
 import { ResponseMedia, User } from '../model/Twitter'
 
@@ -63,44 +64,7 @@ export default function Index() {
             setIncludeRT={setIncludeRT}
           />
         </div>
-        <div className="absolute top-5 right-5 dark:bg-darkblue">
-          <Switch
-            checked={lightMode}
-            onChange={setLightMode}
-            className={`bg-gray-200 relative inline-flex flex-shrink-0 border-white 
-          h-10 w-16 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
-          >
-            <span
-              aria-hidden="true"
-              className={`${lightMode ? 'translate-x-7  ' : 'translate-x-1'}
-            pointer-events-none bg-white inline-block h-7 w-7 mt-1  rounded-full   shadow-lg transform ring-0 transition ease-in-out duration-200`}
-            >
-              {lightMode ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className={`m-1  text-darkblue stroke-current w-5 h-5`}
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-5 h-5 m-1 fill-current text-darkblue"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-                </svg>
-              )}
-            </span>
-          </Switch>
-        </div>
+        <Theme lightMode={lightMode} setLightMode={setLightMode} />
         {error ? (
           <div className="py-20 dark:text-white">
             <div className="border-b">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 import { useState } from 'react'
-import { Switch } from '@headlessui/react'
-import { ResponseMedia } from '../model/Twitter'
 import { Search } from '../components/search'
 import { useContext } from 'react'
 import { lightModeContext } from '../context/theme'
 import { useRouter } from 'next/dist/client/router'
+import { Theme } from '../components/theme'
 
 export default function Home() {
   const router = useRouter()
 
   const { lightMode, setLightMode } = useContext(lightModeContext)
-  const [tweets, setTweets] = useState<ResponseMedia[]>([])
   const [userName, setUserName] = useState('')
   const [includeRT, setIncludeRT] = useState(false)
 
@@ -30,45 +28,7 @@ export default function Home() {
             setIncludeRT={setIncludeRT}
           />
         </div>
-        <div className="absolute top-5 right-5 dark:bg-darkblue">
-          <Switch
-            checked={lightMode}
-            onChange={setLightMode}
-            className={`bg-gray-200 relative inline-flex flex-shrink-0 border-white 
-          h-10 w-16 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
-          >
-            <span
-              aria-hidden="true"
-              className={`${lightMode ? 'translate-x-7  ' : 'translate-x-1'}
-            pointer-events-none bg-white inline-block h-7 w-7 mt-1  rounded-full   shadow-lg transform ring-0 transition ease-in-out duration-200`}
-            >
-              {lightMode ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className={`m-1  text-darkblue stroke-current w-5 h-5`}
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-5 h-5 m-1 fill-current text-darkblue"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-                </svg>
-              )}
-            </span>
-          </Switch>
-        </div>
-
+        <Theme lightMode={lightMode} setLightMode={setLightMode} />
         <div className="p-10">
           <div className="flex justify-center mt-10 mb-20 ">
             <button
