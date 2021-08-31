ba572425be347d4d32efa999bdabf8b273f4f635
Nishino0719 <nishino0719@icloud.com>
2021-08-31 09:53:08 +0900

    :hammer: add init Twitter

---
+++ src/lib/Twitter.ts
@@ -0,0 +1,10 @@
+import Twitter from 'twitter-lite'
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  bearer_token: process.env.TWITTER_BEARER_TOKEN
+})
+
+export default client
